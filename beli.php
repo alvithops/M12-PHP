4baa776cf5804cf5d4e0ac1cab3217b3d2674b0c
modul10 <alvithops@gmail.com>
2026-02-05 21:52:04 +0700

    Update konfigurasi profile dan tambah konten 42

--- beli.php
+++ beli.php
@@ -2,7 +2,7 @@
 /* ===============================
    KONEKSI DATABASE
    =============================== */
-$koneksi = mysqli_connect("localhost", "root", "", "tokoblaas");
+$koneksi = mysqli_connect("localhost", "root", "", "tokoblaa");
 if (!$koneksi) {
     die("Koneksi database gagal");
 }
