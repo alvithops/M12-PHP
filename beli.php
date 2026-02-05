bc1ca03979ecc4f15636e8cafd18b141f130520a
modul10 <alvithops@gmail.com>
2026-02-05 21:37:16 +0700

    Update konfigurasi profile dan tambah konten 21

--- beli.php
+++ beli.php
@@ -169,7 +169,7 @@ $ids = array_filter(array_map('intval', explode(",", $barangPilih)), fn($v) => $
             }
             echo "</table>";
         } else {
-            echo "<p>kera kosong.</p>";
+            echo "<p>keranjang kosong.</p>";
         }
         ?>
 
