ec038576c05787ace509e0764c0aa29b4e40839b
modul10 <alvithops@gmail.com>
2026-02-05 21:47:01 +0700

    Update konfigurasi profile dan tambah konten 34

--- beli.php
+++ beli.php
@@ -2,7 +2,7 @@
 /* ===============================
    KONEKSI DATABASE
    =============================== */
-$koneksi = mysqli_connect("localhost", "root", "", "o");
+$koneksi = mysqli_connect("localhost", "root", "", "toko");
 if (!$koneksi) {
     die("Koneksi database gagal");
 }
